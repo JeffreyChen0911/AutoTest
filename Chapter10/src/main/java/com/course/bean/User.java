0b20cfec029f286e6d0e26f7837bc884288a3afd
chenjw9 <chenjw9@asiainfo.com>
2021-11-04 01:05:14 +0800

    测试idea提交

--- Chapter10/src/main/java/com/course/bean/User.java
+++ Chapter10/src/main/java/com/course/bean/User.java
@@ -4,4 +4,9 @@ import lombok.Data;
 
 @Data
 public class User {
+    private  String userName;
+    private  String password;
+    private  String name;
+    private  String age;
+    private  String sex;
 }
--- Chapter10/src/main/java/com/course/server/MyPostMethod.java
+++ Chapter10/src/main/java/com/course/server/MyPostMethod.java
@@ -1,12 +1,12 @@
 package com.course.server;
 
+import com.course.bean.User;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
@@ -18,6 +18,7 @@ public class MyPostMethod {
 
     //用户登录登录成功获取到cookies，然后再访问其他接口获取到列表
     @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ApiOperation(value = "登录接口，成功后后去Cookies信息", httpMethod = "POST")
     public String login(HttpServletResponse response,
                         @RequestParam(value = "userName", required = true) String userName,
                         @RequestParam(value = "password", required = true) String password){
@@ -27,6 +28,30 @@ public class MyPostMethod {
             return "恭喜你登录成功了！";
         }
         return "用户名或者是密码错误！";
+    }
+
+    @RequestMapping(value = "/getUserList", method = RequestMethod.POST)
+    @ApiOperation(value = "获取用户列表", httpMethod = "POST")
+    public String getUerList(HttpServletRequest request,
+                           @RequestBody User u){
+        User user;
+        //获取cookies
+        Cookie[] cookies = request.getCookies();
+        //验证cookies是否合法
+        for (Cookie c : cookies) {
+            if(c.getName().equals("login")
+                    && c.getValue().equals("true")
+                    && u.getUserName().equals("zhangsan")
+                    && u.getPassword().equals("123456")
+                    ){
+                user = new User();
+                user.setName("lisi");
+                user.setAge("18");
+                user.setSex("man");
+                return user.toString();
+            }
+        }
+        return "参数不合法";
     }
 
 }
